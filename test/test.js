199166732fd86c0bc174e646b59f1a764c4b6c57
Joe McCann <joseph.isaac@gmail.com>
2013-01-21 18:32:27 -0500

    added some test

---
+++ test/test.js
@@ -0,0 +1,104 @@
+var ncr = require('nodecr')
+  , request = require('request')
+  , path = require('path')
+  , mocha = require('mocha')
+  , should = require('should')
+  , ristretto = require('../lib/ristretto')
+  , goodImgUrl = 'https://www.google.com/images/srpr/logo3w.png' 
+  , notImgUrl = 'https://www.google.com' 
+  , brokenUrl = 'http://asdfkljaklsdfj.com'
+  , notUrl = 'foo-bar.baz'
+
+String.prototype.trim = function () {
+   return this.replace(/^\s+/, '').replace(/\s+$/, '');
+ }
+
+ristretto.setImagesDirectory( path.resolve(__dirname, '..', 'images') )
+
+
+describe('Ristretto', function(){
+  describe('#ocrFromUrl', function(){
+    
+    it('Should be equal to the text "Google"', function(done){
+      
+      ristretto.ocrFromUrl(goodImgUrl, function(err,data){
+        if(err){
+          return done(err)
+        }
+        else{
+           data = data.trim()
+           should.strictEqual(data, "Google")
+           done()
+
+        } // end else
+
+      }) // end ocrFromUrl
+      
+      
+    }) // end it
+  }) // end describe
+}) // end describe outer
+
+
+describe('Ristretto', function(){
+  describe('#ocrFromUrl', function(){
+    
+    it('Should match the error message of not being a proper URL.', function(done){
+      
+      ristretto.ocrFromUrl(notUrl, function(err,data){
+        if(err){
+          should.equal(err.message, 'URL is not a URL')
+          done()
+        }
+        else{
+          done(new Error('This works?'))
+        }
+
+      })
+      
+    }) // end it
+  }) // end describe
+}) // end describe outer
+
+
+describe('Ristretto', function(){
+  describe('#ocrFromUrl', function(){
+    
+    it('Should match the error message of not being an image.', function(done){
+      
+      ristretto.ocrFromUrl(notImgUrl, function(err,data){
+        if(err){
+          should.equal(err.message, 'This is not an image according to its content-type header.')
+          done()
+        }
+        else{
+          done(new Error('This works?'))
+        }
+
+      })
+      
+    }) // end it
+  }) // end describe
+}) // end describe outer
+
+
+describe('Ristretto', function(){
+  describe('#ocrFromUrl', function(){
+    
+    it('Should match the error of a bad url.', function(done){
+      
+      // // This url is purposely borked and doens't work
+      ristretto.ocrFromUrl(brokenUrl, function(err,data){
+        if(err){
+          should.equal(err.code, "ENOTFOUND")
+          done()
+        }
+        else{
+          done(new Error('This Works?'))
+        }
+
+      })
+            
+    }) // end it
+  }) // end describe
+}) // end describe outer
